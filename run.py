becc833b14c368df525754deefe5899cceb34ce4
Shiyas9961 <shiyasshiyyu9961@gmail.com>
2025-02-01 23:20:05 +0530

    feat: added port number through environment

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
+PORT = os.environ.get("PORT")
 
 @app.route("/")
 def run():
     return jsonify({"message": "Hello, World!"})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=PORT)
